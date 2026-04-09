124b0b504ba33265c322d79d5632655fb456382c
siddhanta sahoo <siddhanta027z@gmail.com>
2026-04-09 22:44:59 +0530

    adding the intercepter

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,4 +5,20 @@ const axiosInstance = axios.create({
   withCredentials: true, // by adding this field browser will send the cookies to server automatically, on every single req
 });
 
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    // Clerk attaches itself to the global window object in React
+    if (window.Clerk && window.Clerk.session) {
+      const token = await window.Clerk.session.getToken();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 export default axiosInstance;
